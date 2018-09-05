4a3f722043b9bf8a27a0d08f4f49ccb7c923fad8
Zijian Xu <xuzijian@g.ecc.u-tokyo.ac.jp>
2018-09-05 17:23:28 +0900

    Retry when failed

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,15 @@
 const puppeteer = require('puppeteer');
 const cron = require('cron').CronJob;
 
+// https://stackoverflow.com/questions/9768444/possible-eventemitter-memory-leak-detected
+require('events').EventEmitter.defaultMaxListeners = 0
+
 const id = {
   'xuzijian629': 'U2F6UPCKB',
   'kenshin': 'U2F7L8BGE',
-  'ScarletBat': 'U2F5TUURF',
-  'szkieletor': 'U2F6AMVDG',
-  'tomcatowl': 'U2F7K0FPX'
+  'ScarletBat': 'U2F5TUURF'
+  // 'szkieletor': 'U2F6AMVDG',
+  // 'tomcatowl': 'U2F7K0FPX'
 };
 let latestPointsum = {};
 let mute = {};
@@ -17,7 +20,7 @@ async function getSolvedProblems(user, date, update) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(`https://kenkoooo.com/atcoder/?user=${user}&kind=user`, {waitUntil: "networkidle2", timeout: 3000000});
-    await page.waitFor(120000);
+    await page.waitFor(60000);
     let streak = await page.$('#root > div > div > div > div > div:nth-child(3) > div:nth-child(7) > h3');
     ret['streak'] = await (await streak.getProperty('textContent')).jsonValue();
     let longestStreak = await page.$('#root > div > div > div > div > div:nth-child(3) > div:nth-child(6) > h3');
@@ -58,11 +61,16 @@ async function getSolvedProblems(user, date, update) {
   return ret;
 }
 
-async function notifyIfUnsolved(robot, user, message) {
+async function notifyIfUnsolved(robot, user, message, retry) {
   try {
     if (mute[id[user]]) return;
     const today = (new Date(Date.now() + 9 * 3600000)).toISOString().slice(0,10);
     let solved = await getSolvedProblems(user, today, false);
+    if (solved['solved'].length === 0 && retry > 0) {
+      console.log(`retrying for ${user}`);
+      notifyIfUnsolved(robot, user, message, retry - 1);
+      return;
+    }
     console.log(`${user} solved:`);
     console.log(solved);
     if (solved['solved'].length === 0) {
@@ -75,13 +83,19 @@ async function notifyIfUnsolved(robot, user, message) {
     }
   } catch(e) {
     console.error(e);
+    notifyIfUnsolved(robot, user, message, retry - 1);
   }
 }
 
-async function summarize(robot, user) {
+async function summarize(robot, user, retry) {
   try {
     const today = (new Date(Date.now() + 9 * 3600000 - 600000)).toISOString().slice(0,10);
     let solved = await getSolvedProblems(user, today, true);
+    if (solved['solved'].length === 0 && retry > 0) {
+      console.log(`retrying for ${user}`);
+      summarize(robot, user, retry - 1);
+      return;
+    }
     console.log(`${user} solved:`);
     console.log(solved);
     if (solved['solved'].length) {
@@ -101,13 +115,14 @@ async function summarize(robot, user) {
     }
   } catch(e) {
     console.error(e);
+    summarize(robot, user, retry - 1);
   }
 }
 
 async function init() {
   const today = (new Date(Date.now() + 9 * 3600000)).toISOString().slice(0,10);
   for (let user in id) {
-    await getSolvedProblems(user, today, true);
+    await getSolvedProblems(user, today, true, 2);
   }
 }
 
@@ -124,34 +139,34 @@ module.exports = robot => {
     await init();
   })();
 
-  new cron('0 8 0 * * *', () => {
+  new cron('0 9 0 * * *', () => {
     for (let user in id) {
       !(async() => {
-        await summarize(robot, user);
+        await summarize(robot, user, 2);
       })();
     }
   }, null, true, 'Asia/Tokyo');
 
-  new cron('0 58 19 * * *', () => {
+  new cron('0 59 19 * * *', () => {
     for (let user in id) {
       !(async() => {
-        await notifyIfUnsolved(robot, user, 'AtCoderやれ');
+        await notifyIfUnsolved(robot, user, 'AtCoderやれ', 2);
       })();
     }
   }, null, true, 'Asia/Tokyo');
 
-  new cron('0 58 21 * * *', () => {
+  new cron('0 59 21 * * *', () => {
     for (let user in id) {
       !(async() => {
-        await notifyIfUnsolved(robot, user, 'そろそろAtCoderやれ');
+        await notifyIfUnsolved(robot, user, 'そろそろAtCoderやれ', 2);
       })();
     }
   }, null, true, 'Asia/Tokyo');
 
-  new cron('0 58 22 * * *', () => {
+  new cron('0 59 22 * * *', () => {
     for (let user in id) {
       !(async() => {
-        await notifyIfUnsolved(robot, user, 'いい加減AtCoderやれ');
+        await notifyIfUnsolved(robot, user, 'いい加減AtCoderやれ', 2);
       })();
     }
   }, null, true, 'Asia/Tokyo');
